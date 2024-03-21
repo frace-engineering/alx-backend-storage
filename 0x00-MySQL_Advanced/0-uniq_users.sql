afb9d2de2c40a5b85f967ab900012315a44ab08c
Engr Ugwu <fugwungene@gmail.com>
2024-03-21 17:01:37 +0000

    Use INTEGER instead of INT.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create a table named users
 -- IF table exists, should not throw error
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
        	email VARCHAR(255) NOT NULL UNIQUE,
        	name VARCHAR(255) NOT NULL
 );  
