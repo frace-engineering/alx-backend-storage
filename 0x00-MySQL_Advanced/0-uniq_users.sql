68a7757327b5d140b54b0629c46ebaa3054a605a
Engr Ugwu <fugwungene@gmail.com>
2024-03-21 16:42:27 +0000

    Add unique key work.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- IF table exists, should not throw error
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY,
-       	email VARCHAR(255) NOT NULL,
+       	email VARCHAR(255) UNIQUE NOT NULL,
        	name VARCHAR(255) NOT NULL,
 );  
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- IF table exists, should not throw error
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY,
-       	email VARCHAR(255) NOT NULL,
+       	email VARCHAR(255) UNIQUE NOT NULL,
        	name VARCHAR(255) NOT NULL,
 	country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US"
 );  
