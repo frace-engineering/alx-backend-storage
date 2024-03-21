ce30d17736c55c79e6456044a96ac53296d07b53
Engr Ugwu <fugwungene@gmail.com>
2024-03-21 16:46:26 +0000

    Modify code.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create a table named users
 -- IF table exists, should not throw error
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
        	email VARCHAR(255) UNIQUE NOT NULL,
        	name VARCHAR(255) NOT NULL,
 );  
