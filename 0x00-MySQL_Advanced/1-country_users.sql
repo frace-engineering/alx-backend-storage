951105028d98c60e55081bc60158ef6b37f3830f
Engr Ugwu <fugwungene@gmail.com>
2024-03-21 17:05:16 +0000

    Remove the NOT NULL on name attribute.

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@
 CREATE TABLE IF NOT EXISTS users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
        	email VARCHAR(255) UNIQUE NOT NULL,
-       	name VARCHAR(255) NOT NULL,
+       	name VARCHAR(255),
 	country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US"
 );  
