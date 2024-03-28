b818cd18aedc9308a26c548643bfe95ea072a33e
Engr Ugwu <fugwungene@gmail.com>
2024-03-28 04:53:37 +0000

    Add task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#1/usr/bin/env python3
 """
-Redis class method and a random key generator.
+Redis class method and a random key generator
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
+
 
 class Cache:
     """Initialize the cache class"""
@@ -17,3 +18,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
+
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, int)
