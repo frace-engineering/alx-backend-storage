84e003c72587175aec734fecba9d7c0bf2c7e250
Engr Ugwu <fugwungene@gmail.com>
2024-03-28 04:39:09 +0000

    Include the sheband line and comment.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python3
+"""
+Redis class method and a random key generator.
+"""
 import redis
 import uuid
 from typing import Union
 
 class Cache:
+    """Initialize the cache class"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Take data argument and return a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
